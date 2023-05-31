59fba4a57b6e31c27fc9e62ae4a70c0c90525193
zderev <qbeat@bk.ru>
2023-05-31 21:01:13 +0300

    OUT8 Ready

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -10,10 +10,5 @@ void init(Plugin* p) {
     p->addModel(modelVCA);
     p->addModel(modelNotes);
     p->addModel(modelPIN8);
-
-	// Add modules here
-	// p->addModel(modelMyModule);
-
-	// Any other plugin initialization may go here.
-	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+    p->addModel(modelOUT8);
 }
